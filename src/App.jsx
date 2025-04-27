4727d83d57755055c9b28af1c9271e5930541878
Sara <sarailic431@gmail.com>
2025-04-27 14:10:47 +0200

    app info

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,29 @@
 import { useState } from "react";
 import "./App.css";
 
+const info = [
+  {
+    name: "Samsung Galaxy S8",
+    price: "399.99",
+    image: "./phone-1.png",
+  },
+  {
+    name: "Google Pixel",
+    price: "499.99",
+    image: "./phone-2.png",
+  },
+  {
+    name: "Xiaomi Redmi Note 2",
+    price: "699.99",
+    image: "./phone-3.png",
+  },
+  {
+    name: "Samsung Galaxy S7",
+    price: "$599.99",
+    image: "./phone-3.png",
+  },
+];
+
 function App() {
   return (
     <>
